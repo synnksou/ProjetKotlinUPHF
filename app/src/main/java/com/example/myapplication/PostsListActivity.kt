b0b0d271645cd4f33e9e59728a12be512141ba20
Arthur <arthur.bruniau@gmail.com>
2020-06-14 00:50:49 +0200

    retire excedent

--- app/src/main/java/com/example/myapplication/PostsListActivity.kt
+++ app/src/main/java/com/example/myapplication/PostsListActivity.kt
@@ -94,30 +94,6 @@ class PostsListActivity : AppCompatActivity() {
         })
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            R.id.menu_sign_out ->{
-                FirebaseAuth.getInstance().signOut()
-                val intent = Intent(this, MainActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
 
@@ -134,11 +110,3 @@ class PostItem(private val post: Post): Item<ViewHolder>() {
         return R.layout.post_row
     }
 }
-
-// this is super tedious
-
-//class CustomAdapter: RecyclerView.Adapter<ViewHolder> {
-//  override fun onBindViewHolder(p0:, p1: Int) {
-//    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//  }
-//}
